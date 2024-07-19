8fc4449598a11a608d9af682e941f7748a709040
Enzomoloi <enzoomouraa85@gmail.com>
2024-07-18 21:44:27 -0300

    feat:models.py

--- autocare/models.py
+++ autocare/models.py
@@ -22,6 +22,9 @@ class User:
     created_at: Mapped[datetime] = mapped_column(init=False, server_default=func.now())
     is_active: Mapped[bool] = mapped_column(default=False)
 
+    adrress: Mapped["Adrress"] = relationship(back_populates="user")
+    vehicle_infomation: Mapped["Vehicle_Information"] = relationship(back_populates="user")
+
 class Address:
     __tablename__ = "adrress"
 
@@ -32,6 +35,10 @@ class Address:
     city: Mapped[str] = mapped_column(String(50))
     state: Mapped[str] = mapped_column(String(50))
     postal_code: Mapped[str] = mapped_column()
+    
+    user: Mapped["User"] = relationship(back_populates="address")
+    mechanics: Mapped["Mechanics"] = relationship(back_populates="address")
+
 
 class Mechanics:
     __tablename__ = "mechanics"
@@ -44,6 +51,9 @@ class Mechanics:
     close_mechanics: Mapped[datetime] = mapped_column()
     created_at: Mapped[datetime] = mapped_column(initi=False, sever_default=func.now())
 
+    user: Mapped["User"] = relationship(back_populates="mechanics")
+         
+
 class Veicle_Information:
     __tablename__ = "veicle_information"
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
@@ -54,12 +64,18 @@ class Veicle_Information:
     chassis: Mapped[str] = mapped_column(String(20))
     current_mileage: Mapped[int] = mapped_column()
 
+    user: Mapped["User"] = relationship(back_populates="veicle_information")
+
+
 class Service_Detais:
     __tablebname__ = "service_detais"
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
     service_date: Mapped[datetime] = mapped_column()
     service_description: Mapped[str] = mapped_column()
     reason_for_service: Mapped[str] = mapped_column()
+
+    mechanics: Mapped["Mechanics"] = relationship(back_populates="addresses")
+
 
 class Diagnosis:
     __tablename__ = "diagnosis"
@@ -68,6 +84,8 @@ class Diagnosis:
     causes: Mapped[str] = mapped_column(String(150))
     solutions: Mapped[str] = mapped_column(String(150))
 
+    service_detais: Mapped["Service_Detais"] = relationship(back_populates="diagnosis")
+
 class Payment:
     __tablename__ = "payment"
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
@@ -75,6 +93,8 @@ class Payment:
     intallment_plan: Mapped[int] = mapped_column()
     payment_date: Mapped[datetime] = mapped_column()
     payment_method: Mapped[str] = mapped_column(String(15))
+
+    service_detais: Mapped["Service_Detais"] = relationship(back_populates="payment")
 
 
     
